af1215dc199c992f200f2fca1e80217dac073baa
Stefan <steevie.brandl@gmail.com>
2025-04-04 17:40:12 +0200

    caching update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'standalone',
+    poweredByHeader: false,
+    compress: true,
+
+    // Diese Option wurde verschoben
+    outputFileTracingExcludes: {
+        '*': [
+            'node_modules/@swc/core-linux-x64-gnu',
+            'node_modules/@swc/core-linux-x64-musl',
+        ],
+    },
+
+    images: {
+        formats: ['image/avif', 'image/webp'],
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    },
+
+    experimental: {
+        optimizeCss: true,
+        optimizeServerReact: true,
+    },
 };
 
 export default nextConfig;
